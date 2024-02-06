870a77f2de1feddba629ad321512ba226be06069
andreubp <andreu.bp@gmail.com>
2024-02-06 16:54:32 +0100

    version 1.0

---
+++ ghosts.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import params
+import tokens
+import gspread
+from datetime import datetime
+from gspread_dataframe import set_with_dataframe
+import redcap
+
+def define_ghost_records():
+    ghost_entries = pd.DataFrame(columns=['project','record_id','study_number'])
+    g = 0
+    for token in tokens.REDCAP_PROJECTS_ICARIA.keys():
+        print(token)
+        project = redcap.Project(tokens.URL,tokens.REDCAP_PROJECTS_ICARIA[token])
+
+        df = project.export_records(format='df',fields=['study_number'],events=['epipenta1_v0_recru_arm_1'])
+
+        record_ids = list(df.reset_index()['record_id'])
+        c=4
+        if token in params.prefix_three:
+            c = 3
+        prefix_records = [str(i)[0:c] for i in list(record_ids)]
+        count = 0
+        for prefix in prefix_records:
+            if prefix != params.dict_prefixes[token]:
+                ghost_entries.loc[g] = token,record_ids[count],df['study_number'][record_ids[count]].values[0]
+                g+= 1
+                print("\t",record_ids[count],df['study_number'][record_ids[count]].values[0])
+            count += 1
+
+    ghost_entries.loc[g] = str(datetime.today()),'',''
+    file_to_drive(tokens.ghost_drive_worksheet_name,ghost_entries,
+                  tokens.ghost_drive_filename,tokens.ghost_drive_folder,
+                  index_included=False)
+
+    print("\n\nScript Completed on "+ str(datetime.today()))
+
+
+def file_to_drive(worksheet,df,drive_file_name,folder_id,index_included=True):
+    gc = gspread.oauth(tokens.path_credentials)
+    sh = gc.open(title=drive_file_name,folder_id=folder_id)
+    set_with_dataframe(sh.worksheet(worksheet), df,include_index=index_included)
---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+""" Python script to manage pontential ghost switches between REDCap projects.
+Part of the ICARIA Clinical Trial"""
+
+import ghosts
+
+__author__ = "Andreu Bofill Pumarola"
+__copyright__ = "Copyright 2023, ISGlobal Maternal, Child and Reproductive Health"
+__credits__ = ["Andreu Bofill Pumarola"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__date__ = "20240206"
+__maintainer__ = "Andreu Bofill"
+__email__ = "andreu.bofill@isglobal.org"
+__status__ = "Dev"
+
+
+if __name__ == '__main__':
+    ghosts.define_ghost_records()
